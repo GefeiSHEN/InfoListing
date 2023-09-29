f6b769c5ab900ec483ce030eb3efd60bef9edeae
Gefei Shen <35111787+GefeiSHEN@users.noreply.github.com>
2023-09-29 12:30:17 -0500

    Initial Commit

---
+++ InfoListing/InfoListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoListingApp.swift
+//  InfoListing
+//
+//  Created by Gefei Shen on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct InfoListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
